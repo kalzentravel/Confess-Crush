03365b7840ec484f3258b4fe5192fdd9a76c2a3b
KalzenTravel <60466700+kalzentravel@users.noreply.github.com>
2021-10-19 11:02:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Happy 20/10",
+    introTitle: 'Xin chào Xiu Chen',
+    introDesc: `Ngày Phụ nữ Việt Nam 20/10 là ngày đặc biệt dành cho em. Chúc em một ngày 20/10 đầy ắp niềm vui và hạnh phúc.`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Anh hỏi em phải trả lời thật lòng nhé 🥰',
+    desc: 'Anh có thích anh không ???? ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Ơ Thích thật à, thế có Yêu không <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/photo/?fbid=4202551506519144&set=a.129582783816057' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
